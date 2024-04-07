171116efc96136e96b07216770f07403f63eb874
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-04-07 09:40:39 +0800

    streamlined process.py

--- process.py
+++ process.py
@@ -16,99 +16,108 @@
 
 import subprocess
 import csv
+import sys
 
 account="assets:bank:ocbc_sid"
 date="2023/05"
 
-# 1 journal to csv
-output = subprocess.run(["hledger", "print", account,  "-p", date, "-O", "csv"], capture_output=True, text=True)
-csv_data = output.stdout 
-
-lines = csv_data.split('\n')
-data = []
-
-for l in lines:
-    if account not in l:
-        data.append(l)
-
-# 2 parse csv and load into memory
 class Transaction:
-    def __init__(self, txn_id, date, code, description, comment, account, credit, debit):
-        self.txn_id = txn_id
-        self.date = date
-        self.description = description
-        self.credit = credit
-        self.debit = debit
-        self.account = account
-        self.comment = comment
-        self.code = code
+    def __init__(self, cols):
+        self.txn_id = cols[0]
+        self.date = cols[1]
+        self.code = cols[4]
+        self.description = cols[5]
+        self.comment = cols[6]
+        self.account = cols[7]
+        self.credit = cols[10]
+        self.debit = cols[11]
         self.keep_account = False
 
     def __str__(self):
         return f"{self.txn_id}, {self.date}, {self.description}, {self.credit}, {self.debit}, {self.account}, {self.comment}, {self.code}"
 
+    def __repr__(self):
+        return f"id={self.txn_id}, date={self.date}, desc={self.description}, credit={self.credit}, debit={self.debit}, account={self.account}, comment={self.comment}, code={self.code}"
 
-all_txns = []
-for l in data[1:]:
-    if len(l) == 0:
-        continue
-    cols = l.split(',')
-    txn = Transaction(cols[0], cols[1], cols[4], cols[5], cols[6], cols[7], cols[10], cols[11])
-    all_txns.append(txn)
+    def __eq__(self, other):
+        return self.description == other.description and self.credit == other.credit and self.debit == other.debit and self.account == other.account and self.comment == self.comment and self.code == self.code
 
-# for t in all_txns:
-#     print(t)
+def txns_from_csv(command):
+    csv_file = subprocess.run(command, capture_output=True, text=True)
+    lines = csv_file.stdout.split('\n')
 
-# 3 re-create without accounts
+    header = lines[0]
+    txns = []
+    for l in lines[1:]:
+        if len(l) == 0:
+            continue
+        cols = l.split(',')
+        txn = Transaction(cols)
+        txns.append(txn)
+
+    return header, txns
+
+# 1 journal to csv
+# 2 parse csv and load into memory
+header, all_txns = txns_from_csv(["hledger", "print", account,  "-p", date, "-O", "csv"])
+filtered_txns = [t for t in all_txns if account not in t.account]
+
+# 3 re-create csv without accounts
 filename = '202305_ocbc_sid.csv' 
 tmp_filename = 'tmp_' + filename
 with open(tmp_filename,'w', newline='') as f:
     f.write("txn_id,date,description,credit,debit,account,comment,code\n")
-    for txn in all_txns:
+    for txn in filtered_txns:
         f.write(f"{txn.txn_id},{txn.date},{txn.description},{txn.credit},{txn.debit},,{txn.comment},{txn.code}\n")
 
 
 # [ ] 4 import csv with hledger and create new csv
-output = subprocess.run(["hledger", "print", "-f", tmp_filename, "--rules-file", "ocbc.rules", "-O", "csv"], capture_output=True, text=True)
-
 # [ ] 5 parse csv
-lines = output.stdout.split('\n')
-unknowns = []
+_, txns = txns_from_csv(["hledger", "print", "-f", tmp_filename, "--rules-file", "ocbc.rules", "-O", "csv"])
 
 # [ ] 6 filter out unknown txns
-unknown_txns = []
-for l in lines:
-    if "unknown" in l:
-        cols = l.split(",")
-        txn = Transaction(cols[0], cols[1], cols[4], cols[5], cols[6], cols[7], cols[10], cols[11])
-        unknown_txns.append(txn)
-
+unknown_txns = [t for t in txns if "unknown" in t.account]
 
 def txn_uniq_id(txn):
     return txn.date + txn.description + txn.credit + txn.debit
 
-def find_txn_idx(txn, all_txns):
+def find_txn_idx(txn, txns):
     uid = txn_uniq_id(txn)
-    for idx, t in enumerate(all_txns):
+    for idx, t in enumerate(txns):
         if uid == txn_uniq_id(t):
             return idx 
     return -1
 
 # 7
 for ut in unknown_txns:
-    idx = find_txn_idx(ut, all_txns)
+    idx = find_txn_idx(ut, filtered_txns)
     if idx != -1:
-        all_txns[idx].keep_account = True
+        filtered_txns[idx].keep_account = True
+        print("unknowns found:", filtered_txns[idx])
 
 
 with open(filename,'w', newline='') as f:
-    for t in all_txns:
+    f.write(header+"\n")
+    for t in filtered_txns:
         if not t.keep_account:
             f.write(f"{t.txn_id},{t.date},{t.description},{t.credit},{t.debit},,{t.comment},{t.code}\n")
         else:
             f.write(f"{t.txn_id},{t.date},{t.description},{t.credit},{t.debit},{t.account},{t.comment},{t.code}\n")
 
-# with open(filename,'w', newline='') as f:
-#     for r in result:
-#         print(r)
-#         f.write(f"{r.txn_id},{r.date},{r.description},{r.credit},{r.debit},,{r.comment},{r.code}\n")
+# error checking
+
+_, original_txns = txns_from_csv(["hledger", "print", account,  "-p", date, "-O", "csv"])
+_, new_txns = txns_from_csv(["hledger", "print", "-f", filename, "--rules-file", "ocbc.rules", "-O", "csv"])
+
+if len(original_txns) != len(new_txns):
+    print("unequal no. of txns")
+    print(len(original_txns), len(new_txns))
+    sys.exit(1)
+
+for idx, t in enumerate(original_txns):
+    if t != new_txns[idx]:
+        print("unequal txns", idx)
+        print(t) 
+        print(new_txns[idx])
+        sys.exit(1)
+
