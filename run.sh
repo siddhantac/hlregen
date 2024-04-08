614d9a08ebeb8ec6a992e3b96cdcaa69c9b0c44e
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-04-08 17:06:56 +0800

    iterate over months and ask for confirmation after each

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+dates=( "2023/01" "2023/02" "2023/03" "2023/04" )
+
+CONTINUE=1
+for date in "${dates[@]}"
+do
+    if [ $CONTINUE -eq 0 ]; then
+        exit 0
+    fi
+
+    echo ""
+    echo "---"
+    ./process.py --account "assets:bank:ocbc_sid" --date "$date"
+    gum confirm "Continue?" && CONTINUE=1 || CONTINUE=0
+done
