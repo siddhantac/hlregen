20159a2b75976d55b20252e7053db11a08ec3327
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-04-13 15:31:37 +0800

    update script to run all

--- run.sh
+++ run.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-# dates=( "2023/01" "2023/02" "2023/03" "2023/04" )
-dates=( "2023/01" )
+dates=( "2023/01" "2023/02" "2023/03" "2023/04" )
+# dates=( "2023/01" )
+accounts=( "assets:bank:dbs_twisha" "assets:bank:ocbc_sid" "assets:bank:dbs_sid" "liabilities:credit_card:citibank_prem_miles" "liabilities:credit_card:amex" )
 
 CONTINUE=1
 for date in "${dates[@]}"
 do
-    if [ $CONTINUE -eq 0 ]; then
-        exit 0
-    fi
+    # if [ $CONTINUE -eq 0 ]; then
+    #     exit 0
+    # fi
 
-    echo ""
-    echo "---"
-    # ./process.py --account "assets:bank:dbs_twisha" --date "$date"
-    ./process.py --account "liabilities:credit_card:citibank_prem_miles" --date "$date"
-    gum confirm "Continue?" && CONTINUE=1 || CONTINUE=0
+    for acc in "${accounts[@]}"
+    do
+        echo ""
+        echo "---"
+        # ./process.py --account "assets:bank:dbs_twisha" --date "$date"
+        ./process.py --account "$acc" --date "$date"
+        # gum confirm "Continue?" && CONTINUE=1 || CONTINUE=0
+    done
 done
